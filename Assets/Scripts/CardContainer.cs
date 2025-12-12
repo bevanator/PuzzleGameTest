941316e9ea38f2f89fb16e144506c39abafb6d17
Rimon Akhter <rimona.bevan@gmail.com>
2025-12-13 04:14:01 +0600

    Card flip functionality implemented

---
+++ Assets/Scripts/CardContainer.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MemoryGame
+{
+    public class CardContainer : MonoBehaviour
+    {
+        [Header("Layout")]
+        [SerializeField] private int m_Rows = 2;
+        [SerializeField] private int m_Columns = 2;
+        [SerializeField] private float m_Spacing = 10f;
+        [SerializeField] private float m_AspectRatio = 0.7f;
+
+        [Header("Padding")]
+        [SerializeField] private float m_PaddingLeft = 0f;
+        [SerializeField] private float m_PaddingRight = 0f;
+        [SerializeField] private float m_PaddingTop = 0f;
+        [SerializeField] private float m_PaddingBottom = 0f;
+
+        [Header("Cards")]
+        [SerializeField] private List<RectTransform> m_Cards = new List<RectTransform>();
+
+        private RectTransform _rect;
+
+        void Awake()
+        {
+            _rect = GetComponent<RectTransform>();
+        }
+
+        void OnEnable()
+        {
+            UpdateLayout();
+        }
+
+        void OnRectTransformDimensionsChange()
+        {
+            UpdateLayout();
+        }
+
+        public void UpdateLayout()
+        {
+            if (_rect == null) return;
+            if (m_Cards.Count == 0) return;
+
+            float w = _rect.rect.width - m_PaddingLeft - m_PaddingRight;
+            float h = _rect.rect.height - m_PaddingTop - m_PaddingBottom;
+
+            float cellW = (w - (m_Columns - 1) * m_Spacing) / m_Columns;
+            float cellH = (h - (m_Rows - 1) * m_Spacing) / m_Rows;
+
+            float cardW = Mathf.Min(cellW, cellH * m_AspectRatio);
+            float cardH = cardW / m_AspectRatio;
+
+            float gridW = cardW * m_Columns + m_Spacing * (m_Columns - 1);
+            float gridH = cardH * m_Rows + m_Spacing * (m_Rows - 1);
+
+            float offsetX = m_PaddingLeft + (w - gridW) * 0.5f;
+            float offsetY = m_PaddingTop + (h - gridH) * 0.5f;
+
+            int index = 0;
+
+            for (int r = 0; r < m_Rows; r++)
+            {
+                for (int c = 0; c < m_Columns; c++)
+                {
+                    if (index >= m_Cards.Count) return;
+
+                    RectTransform card = m_Cards[index];
+                    card.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, cardW);
+                    card.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, cardH);
+
+                    float x = offsetX + c * (cardW + m_Spacing);
+                    float y = offsetY + r * (cardH + m_Spacing);
+
+                    card.anchoredPosition = new Vector2(x, -y);
+
+                    index++;
+                }
+            }
+        }
+
+        public void SetCards(List<RectTransform> cards)
+        {
+            m_Cards = cards;
+            UpdateLayout();
+        }
+
+        public void SetGrid(int rows, int columns)
+        {
+            m_Rows = rows;
+            m_Columns = columns;
+            UpdateLayout();
+        }
+    }
+}
---
+++ Assets/Scripts/CardSlot.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using DG.Tweening;
+
+namespace MemoryGame
+{
+    public class CardSlot : MonoBehaviour, IPointerClickHandler
+    {
+        [Header("Card")]
+        [SerializeField] private Image m_Image;
+        [SerializeField] private Sprite m_BackSprite;
+        [SerializeField] private Sprite m_FrontSprite;
+        [SerializeField] private float m_FlipDuration = 0.15f;
+        [SerializeField] private float m_ScaleDownFactor = 0.9f;
+
+        public static event Action<CardSlot> ClickedEvent;
+
+        public bool IsRevealed { get; private set; }
+
+        private Sequence _sequence;
+
+
+        private void Awake()
+        {
+            m_Image.sprite = m_BackSprite;
+            m_Image.rectTransform.localScale = Vector3.one;
+        }
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            if (_sequence != null && _sequence.IsPlaying()) return;
+            ClickedEvent?.Invoke(this);
+            PlayFlip(m_FrontSprite, true);
+        }
+
+        public void Reveal()
+        {
+            if (IsRevealed) return;
+            PlayFlip(m_FrontSprite, true);
+        }
+
+        public void Hide()
+        {
+            if (!IsRevealed) return;
+            PlayFlip(m_BackSprite, false);
+        }
+
+        private void PlayFlip(Sprite targetSprite, bool revealed)
+        {
+            _sequence?.Kill();
+            _sequence = DOTween.Sequence();
+
+            _sequence.Append(m_Image.transform.DOScale(m_ScaleDownFactor, m_FlipDuration));
+            _sequence.Append(m_Image.transform.DOScale(1f, m_FlipDuration));
+            _sequence.Append(m_Image.transform.DOScaleX(0f, m_FlipDuration))
+                     .AppendCallback(() => m_Image.sprite = targetSprite);
+            _sequence.Append(m_Image.transform.DOScaleX(1f, m_FlipDuration));
+
+            _sequence.OnComplete(() => IsRevealed = revealed);
+        }
+
+        public void SetFrontSprite(Sprite sprite)
+        {
+            m_FrontSprite = sprite;
+            if (IsRevealed) m_Image.sprite = m_FrontSprite;
+        }
+
+        public void ResetCard()
+        {
+            _sequence?.Kill();
+            IsRevealed = false;
+            m_Image.transform.localScale = Vector3.one;
+            m_Image.sprite = m_BackSprite;
+        }
+    }
+}
